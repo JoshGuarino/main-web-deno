de17b56c2c1661ea86ff425654190e4ce3f47bda
Josh Guarino <josh.guarino@eagleview.com>
2022-11-07 17:32:14 -0500

    gettin too it...

---
+++ islands/Nav.tsx
@@ -0,0 +1,10 @@
+export default function Nav () {
+    return (
+        <nav class="bg-white border-gray-200 px-2 sm:px-4 py-2.5 dark:bg-gray-800">
+            <div class="container flex flex-wrap justify-between items-center mx-auto">
+
+            </div>
+        </nav>
+    );
+}
+
--- routes/index.tsx
+++ routes/index.tsx
@@ -1,12 +1,14 @@
 import { Head } from "$fresh/runtime.ts";
+import Nav from "../islands/Nav.tsx";
 import Counter from "../islands/Counter.tsx";
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Fresh App</title>
+        <title>Josh Guarino</title>
       </Head>
+      <Nav></Nav>
       <div class="p-4 mx-auto max-w-screen-md">
         <img
           src="/logo.svg"
